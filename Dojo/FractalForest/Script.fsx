51cd3c54123158c391565e9ab1a12d61fa6f0542
Giovanni Bassi <giggio@giggio.net>
2016-11-10 12:56:12 -0800

    More improvements

--- Dojo/FractalForest/Script.fsx
+++ Dojo/FractalForest/Script.fsx
@@ -2,8 +2,14 @@
 open System.Drawing
 open System.Windows.Forms
 
+let finalDepth = 15
+let baseWidth = 10.
+let baseLength = 65.
+let baseAngleVariation = 0.11
 let formWidth, formHeight = 1200, 800
+
 let form = new Form(Width = formWidth, Height = formHeight)
+form.WindowState <- FormWindowState.Maximized
 let box = new PictureBox(BackColor = Color.White, Dock = DockStyle.Fill)
 let image = new Bitmap(formWidth, formHeight)
 let graphics = Graphics.FromImage(image)
@@ -14,17 +20,9 @@ let darkGreenBrush = new SolidBrush(Color.FromArgb(22, 91, 19))
 let greenBrush = new SolidBrush(Color.FromArgb(18, 114, 15))
 let brownBrush = new SolidBrush(Color.FromArgb(102, 51, 0))
 let blackBrush = new SolidBrush(Color.FromArgb(0, 0, 0))
-
 box.Image <- image
 form.Controls.Add(box)
 
-type Direction =
-    | Left
-    | Right
-    | Up
-// Compute the endpoint of a line
-// starting at x, y, going at a certain angle
-// for a certain length.
 let endpoint x y angle length =
     x + length * cos angle,
     y + length * sin angle
@@ -41,6 +39,11 @@ let drawLine (target : Graphics) (brush : Brush)
     target.DrawLine(pen, origin, destination)
     x_end, y_end
 
+type Direction =
+    | Left
+    | Right
+    | Up
+
 let draw x y angle length width direction =
     match direction with
         | Left -> drawLine graphics greenBrush x y angle length width
@@ -48,26 +51,23 @@ let draw x y angle length width direction =
         | _ -> drawLine graphics brownBrush x y angle length width
 
 let pi = Math.PI
-let finalDepth = 15
-let width = 10.
-let baseLength = 65.
-let variation = 0.11
 let rec branch depth x y direction baseAngle =
     let depthRatio = ((float)finalDepth - (float)depth + 1.) / (float)finalDepth
-    let newVariation = variation * depthRatio
+    let angleVariation = baseAngleVariation * depthRatio
     let opennessAngleMultiplier = if depth % 2 = 0 then 1.5 else 1.
-    let leftAngleModifier, rightAngleModifier = baseAngle + (newVariation * opennessAngleMultiplier), baseAngle - (newVariation * opennessAngleMultiplier)
+    let leftAngleModifier = baseAngle + (angleVariation * opennessAngleMultiplier)
+    let rightAngleModifier =  baseAngle - (angleVariation * opennessAngleMultiplier)
     let leftAngle, rightAngle =  pi*leftAngleModifier, pi*rightAngleModifier
-    let newWidth = width * ( 1. / ( (float)depth + 1. ) )
-    let rightLength = baseLength * 1. * depthRatio
+    let width = baseWidth * ( 1. / ( (float)depth + 1. ) )
+    let rightLength = baseLength * 0.9 * depthRatio
     let leftLength = if depth % 2 = 1 then rightLength else rightLength * 0.7
-    let leftx, lefty = draw x y leftAngle leftLength newWidth Left
-    let rightx, righty = draw x y rightAngle rightLength newWidth Right
+    let leftx, lefty = draw x y leftAngle leftLength width Left
+    let rightx, righty = draw x y rightAngle rightLength width Right
     let newDepth = depth + 1
     if newDepth <= finalDepth then
         branch newDepth leftx lefty Left leftAngleModifier
         branch newDepth rightx righty Right rightAngleModifier
 
-let trunkEndX, trunkEndY = draw ((float)formWidth/2.) 50. (pi*(0.5)) 100. width Up
+let trunkEndX, trunkEndY = draw ((float)formWidth/2.) 50. (pi*(0.5)) 100. baseWidth Up
 branch 1 trunkEndX trunkEndY Up 0.5
-form.ShowDialog()
+form.Show()
